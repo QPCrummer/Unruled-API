04f88723098b18feb97b50eb48b657e4ef3a5f77
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-11-13 22:37:03 +0100

    Enum test

--- common/src/test/java/mc/recraftors/unruled_api/mixin/TestRulesMixin.java
+++ common/src/test/java/mc/recraftors/unruled_api/mixin/TestRulesMixin.java
@@ -3,7 +3,9 @@ package mc.recraftors.unruled_api.mixin;
 import mc.recraftors.unruled_api.UnruledApi;
 import mc.recraftors.unruled_api.impl.OddIntRuleValidatorAdapter;
 import mc.recraftors.unruled_api.impl.RoundingBehaviour;
+import mc.recraftors.unruled_api.test.TestEnum;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.GameRules.Category;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,11 +17,19 @@ import java.util.Optional;
 public abstract class TestRulesMixin {
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void testRulesInjector(CallbackInfo ci) {
-        UnruledApi.registerLong("test.test1", GameRules.Category.PLAYER, 0,
+        UnruledApi.registerLong("test.test1", Category.PLAYER, 0,
                 i -> ((i & 1) == 0 && i >= 0 && i < 64), i -> Optional.of(i > 62 ? 62 : i < 0 ? 0 : i + 1)
         );
-        UnruledApi.registerInt("test.test2", GameRules.Category.CHAT, 1,
+        UnruledApi.registerInt("test.test2", Category.CHAT, 1,
                 new OddIntRuleValidatorAdapter(RoundingBehaviour.CEILING)
+        );
+        UnruledApi.registerEnum("test.test3", Category.DROPS, TestEnum.class, TestEnum.B,
+                e -> (e.i & 1) == 0,
+                e -> Optional.of(switch (e) {
+                    case A -> TestEnum.B;
+                    case C -> TestEnum.D;
+                    default -> e;
+                })
         );
     }
 }
---
+++ common/src/test/java/mc/recraftors/unruled_api/test/TestEnum.java
@@ -0,0 +1,15 @@
+package mc.recraftors.unruled_api.test;
+
+public enum TestEnum {
+    A(1),
+    B(2),
+    C(3),
+    D(4),
+    ;
+
+    public final int i;
+
+    TestEnum(int i) {
+        this.i = i;
+    }
+}
