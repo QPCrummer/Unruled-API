807a4bdc06cdbfa0edec77b6dd11351923d9aac9
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-11-13 22:21:41 +0100

    Switch redirect for operation wrap

--- common/src/main/java/mc/recraftors/unruled_api/mixin/GameRuleCommandMixin.java
+++ common/src/main/java/mc/recraftors/unruled_api/mixin/GameRuleCommandMixin.java
@@ -1,5 +1,7 @@
 package mc.recraftors.unruled_api.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.brigadier.context.CommandContext;
 import mc.recraftors.unruled_api.utils.EncapsulatedException;
 import net.minecraft.server.command.GameRuleCommand;
@@ -11,10 +13,19 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameRuleCommand.class)
 public abstract class GameRuleCommandMixin {
-    @Redirect(method = "executeSet", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRules$Rule;set(Lcom/mojang/brigadier/context/CommandContext;Ljava/lang/String;)V"))
-    private static void executeSetGameRuleSetRedirect(GameRules.Rule<?> instance, CommandContext<ServerCommandSource> context, String name) throws Exception {
+    @WrapOperation(
+            method = "executeSet",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/GameRules$Rule;set(Lcom/mojang/brigadier/context/CommandContext;Ljava/lang/String;)V"
+            )
+    )
+    private static void executeSetGameRuleSetWrapper(
+            GameRules.Rule<?> instance, CommandContext<ServerCommandSource> context, String name,
+            Operation<Void> original
+    ) throws Exception {
         try {
-            instance.set(context, name);
+            original.call(instance, context, name);
         } catch (EncapsulatedException ex) {
             throw ex.exception;
         }
